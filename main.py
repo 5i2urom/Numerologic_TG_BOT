e75cde619fdf130ea89c95790998af5a06b58425
RomchAss <gameathome2015@gmail.com>
2023-06-04 20:26:45 +0300

    with constants

--- main.py
+++ main.py
@@ -18,8 +18,15 @@ commands = [
 bot.set_my_commands(commands)
 my_commands = [com.command for com in bot.get_my_commands()]
 
-CORRECT_ID = -1
-SKIP_ID = -1
+class BotState:
+    def __init__(self):
+        self.CORRECT_ID = -1
+        self.SKIP_ID = -1
+
+bot_state = BotState()
+
+#CORRECT_ID = -1
+#SKIP_ID = -1
 
 # выполнить команду
 def handle_command(message):
@@ -32,14 +39,13 @@ def handle_command(message):
 # Старт
 @bot.message_handler(commands=['start'])
 def start(message):
-    global CORRECT_ID, SKIP_ID
     # удаление кнопок Верно/Исправить
-    if CORRECT_ID > -1:
-        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=CORRECT_ID, reply_markup=None)
-        CORRECT_ID = -1
-    if SKIP_ID > -1:
-        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=SKIP_ID, reply_markup=None)
-        SKIP_ID = -1
+    if bot_state.CORRECT_ID > -1:
+        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=bot_state.CORRECT_ID, reply_markup=None)
+        bot_state.CORRECT_ID = -1
+    if bot_state.SKIP_ID > -1:
+        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=bot_state.SKIP_ID, reply_markup=None)
+        bot_state.SKIP_ID = -1
 
     go_count = types.InlineKeyboardMarkup()
     go_count_button = types.InlineKeyboardButton(text='Посчитать человека', callback_data='go_count')
@@ -50,15 +56,14 @@ def start(message):
 # Посчитать человека
 @bot.message_handler(commands=['count'])
 def count(message):
-    global CORRECT_ID, SKIP_ID
     # удаление кнопок Верно/Исправить
-    if CORRECT_ID > -1:
-        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=CORRECT_ID, reply_markup=None)
-        CORRECT_ID = -1
+    if bot_state.CORRECT_ID > -1:
+        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=bot_state.CORRECT_ID, reply_markup=None)
+        bot_state.CORRECT_ID = -1
    
-    if SKIP_ID > -1:
-        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=SKIP_ID, reply_markup=None)
-        SKIP_ID = -1
+    if bot_state.SKIP_ID > -1:
+        bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=bot_state.SKIP_ID, reply_markup=None)
+        bot_state.SKIP_ID = -1
 
     bot.send_message(message.chat.id, "Введите ФИО человека")
     bot.register_next_step_handler(message, input_name) # ожидание ввода имени
@@ -73,7 +78,6 @@ def input_name(message):
 
 # Ввод др
 def input_date(message, name): 
-    global SKIP_ID
     if message.text[1:] in my_commands: handle_command(message)
     else: 
         date = message.text
@@ -82,37 +86,34 @@ def input_date(message, name):
             add_info.add(types.InlineKeyboardButton(text = "Пропустить", callback_data = f"skip_{name}_{date}"))
             bot.send_message(message.chat.id, "Введите дополнительную информацию о человеке",
                             reply_markup=add_info)
-            SKIP_ID = message.message_id + 1
+            bot_state.SKIP_ID = message.message_id + 1
             bot.register_next_step_handler(message, input_add, name, date) # ожидание ввода доп. инфы
         else:
             bot.send_message(message.chat.id, "Введите дату рождения человека в формате ДД.ММ.ГГГГ")
             bot.register_next_step_handler(message, input_date, name)
 
 # Ввод доп. инфы
-def input_add(message, name, date, default=False):  
-    global SKIP_ID    
+def input_add(message, name, date, default=False):     
     if message.text[1:] in my_commands: handle_command(message)
     else:
-        if SKIP_ID > -1:
-            bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=SKIP_ID, reply_markup=None)
-            SKIP_ID = -1
+        if bot_state.SKIP_ID > -1:
+            bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=bot_state.SKIP_ID, reply_markup=None)
+            bot_state.SKIP_ID = -1
         add = '-' if default else message.text
         check_info(message, name, date, add)
 
 # Предложение проверить инфу
 def check_info(message, name, date, add):
-    global CORRECT_ID
     check_keyboard = types.InlineKeyboardMarkup()
     check_keyboard.add(types.InlineKeyboardButton(text = "Верно", callback_data = f"correct_{name}_{date}_{add}"))
     check_keyboard.add(types.InlineKeyboardButton(text = "Исправить", callback_data = "change"))
     bot.send_message(message.chat.id, f"ФИО: {name}\nДата рождения: {date}\nДоп. информация: {add}",
                       reply_markup = check_keyboard)
-    CORRECT_ID = message.message_id + 1
+    bot_state.CORRECT_ID = message.message_id + 1
 
 # обработка кнопок
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    global CORRECT_ID, SKIP_ID
     if call.data == "go_count":
         bot.answer_callback_query(call.id) 
         bot.clear_step_handler(call.message)
@@ -121,7 +122,7 @@ def callback_worker(call):
     elif call.data.startswith("skip"):
         bot.answer_callback_query(call.id) 
         bot.clear_step_handler(call.message)
-        SKIP_ID = -1
+        bot_state.SKIP_ID = -1
         data = call.data.split("_")
         name = data[1]
         date = data[2]
@@ -139,7 +140,7 @@ def callback_worker(call):
         bot.clear_step_handler(call.message)
         bot.edit_message_reply_markup(chat_id=call.message.chat.id,
                         message_id=call.message.message_id, reply_markup=None)
-        CORRECT_ID = -1
+        bot_state.CORRECT_ID = -1
         data = call.data.split("_")
         name = data[1]
         date = data[2]
